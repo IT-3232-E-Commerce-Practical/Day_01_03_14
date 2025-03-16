f722dccb2df8bacf064cf6bd0bd620ae7c45c899
TharinduLakshimal <tharindulakshimal@gmail.com>
2025-03-16 17:19:34 +0530

    feat: Implement Library system with different book types

---
+++ Audiobook.java
@@ -0,0 +1,23 @@
+
+public class Audiobook extends Book {
+    private double duration;
+    
+
+    public Audiobook(String title, String author, int publicationYear, double duration) {
+        super(title, author, publicationYear);
+        this.duration = duration;
+        
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+   
+
+    @Override
+    public void displayDetails() {
+        System.out.println("Audio Book: " + getTitle() + " by " + getAuthor() + 
+                           ", Year: " + getPublicationYear() + ", Duration: " + duration + " hours");
+    }
+}
