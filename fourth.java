888095e5a1ac09e4df49aed8ecdf733758f91389
TharinduLakshimal <tharindulakshimal@gmail.com>
2025-03-16 15:45:14 +0530

    print the odd numbers between 1 to 20

---
+++ fourth.java
@@ -0,0 +1,13 @@
+public class fourth {
+    public static void main(String[] args) {
+
+        for(int i=1; i<=20; i++){
+            int num = i%2;
+            if(num!=0){
+            System.out.println(i);
+            }
+        }
+       
+
+    }
+}
